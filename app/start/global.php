1066c846e570aad14839d75b67830eeffa2f9170
spark0123 <sue.park@aetn.com>
2014-03-21 15:16:25 -0400

    upgrade to 4.1 and include Laravel-dropbox
    https://github.com/GrahamCampbell/Laravel-Dropbox

--- app/start/global.php
+++ app/start/global.php
@@ -17,6 +17,7 @@ ClassLoader::addDirectories(array(
 	app_path().'/controllers',
 	app_path().'/models',
 	app_path().'/database/seeds',
+	app_path().'/classes',
 
 ));
 
@@ -27,14 +28,15 @@ ClassLoader::addDirectories(array(
 |
 | Here we will configure the error logger setup for the application which
 | is built on top of the wonderful Monolog library. By default we will
-| build a rotating log file setup which creates a new file each day.
+| build a basic log file setup which creates a single file for logs.
 |
 */
-
-$logFile = 'log-'.php_sapi_name().'.txt';
-
-Log::useDailyFiles(storage_path().'/logs/'.$logFile);
-
+if (App::environment() === 'local') {
+	Log::useFiles(storage_path().'/logs/laravel.log');
+}
+else {
+	Log::useFiles('/var/log/httpd/laravel.log');
+}
 /*
 |--------------------------------------------------------------------------
 | Application Error Handler
@@ -60,7 +62,7 @@ App::error(function(Exception $exception, $code)
 |
 | The "down" Artisan command gives you the ability to put an application
 | into maintenance mode. Here, you will define what is displayed back
-| to the user if maintenace mode is in effect for this application.
+| to the user if maintenance mode is in effect for the application.
 |
 */
 
